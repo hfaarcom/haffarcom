e5fb8cf7fbe54025e5a029ab277dd1e169e88bd5
Omar aldesi <omar.desi.100@gmail.com>
2022-12-21 20:39:58 +0300

    fix database v4

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('MainAdmin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('Admin/', include('Admin.urls')),
+    path('', include('Admin.urls')),
     path('', include('allauth.urls'))
 ]
