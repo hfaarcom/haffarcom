5de98a939e69db8ecff9b6bcc579cee0b01af047
Omar aldesi <omar.desi.100@gmail.com>
2023-01-05 13:34:06 +0300

    fix

--- core/views.py
+++ core/views.py
@@ -274,7 +274,7 @@ def getAllProducts(request):
     try:
         products = Product.objects.all().order_by('-id')
         serializer = ProductsSerilizer(products, many=True)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
